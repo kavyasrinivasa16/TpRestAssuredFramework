80d65dfd7c1d05d9d45b55e9aba12afe1b780006
Lenovo <Lenovo@DESKTOP-6R86IG7>
2025-01-20 17:56:14 +0530

    payment mocking server

---
+++ PaymentMockingServer/src/test/java/Mock/creditCardMock.java
@@ -0,0 +1,30 @@
+package Mock;
+
+import org.apache.commons.lang3.StringUtils;
+import com.jayway.jsonpath.JsonPath;
+import spark.Spark;
+
+public class creditCardMock {
+
+	public static void main(String[] args) {
+
+		Spark.port(8889);
+		Spark.post("/credit-card", (req, res) -> {
+
+			String response = "";
+			String card = JsonPath.read(req.body().toString(), "$.creditcard");
+			if (StringUtils.equalsAny(card, "1234567891234", "1234567891235")) {
+				response = "{\"status\":\"Payment success\"}";
+				res.status(200);
+			} else {
+				response = "{\"status\":\"Payment failed\"}";
+				res.status(404);
+			}
+			res.type("application/json");
+			return response;
+		});
+
+		System.out.println("=======Running......=======");
+	}
+
+}
